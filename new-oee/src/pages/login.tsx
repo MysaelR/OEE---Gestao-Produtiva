47cd40163a517ef7a7f5fb559afc05b049ee75f6
MysaelR <90765797+MysaelR@users.noreply.github.com>
2022-06-08 09:47:17 -0300

    fazendo logo em forma de texto e responsiva

--- new-oee/src/pages/login.tsx
+++ new-oee/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { ContainerLogin, HeaderLogin, PrincipalDivLogin } from "../styles/style-login";
+import { Container, ContainerBase, ContainerLogin, ContainerMed, ContainerTop, FlexDivLogin, GridDivLogin, HeaderLogin, HeaderLoginText, PrincipalDivLogin } from "../styles/style-login";
 
 export default function Login() {
 
@@ -10,9 +10,36 @@ export default function Login() {
         <PrincipalDivLogin>
             <ContainerLogin>
                 <HeaderLogin>
+                    <GridDivLogin>
+                        <FlexDivLogin>
+                            <HeaderLoginText size={7} bolder lHeight={60}>
+                                OEE
+                            </HeaderLoginText>
+                            <HeaderLoginText size={1.72} lHeight={100}>
+                                SISTEMA DE<br />
+                                GESTÃO DA<br />
+                                PRODUÇÃO
+                            </HeaderLoginText>
+                        </FlexDivLogin>
+
+                    </GridDivLogin>
 
                 </HeaderLogin>
             </ContainerLogin>
         </PrincipalDivLogin>
     );
 }
+
+
+
+{/* <Container>
+<ContainerTop>
+    SISTEMA DE
+</ContainerTop>
+<ContainerMed>
+    GESTÃO DA
+</ContainerMed>
+<ContainerBase>
+    PRODUÇÃO
+</ContainerBase>
+</Container> */}
--- new-oee/src/styles/style-login.tsx
+++ new-oee/src/styles/style-login.tsx
@@ -6,6 +6,12 @@ interface InputControllI {
     top?: number
 }
 
+interface TextControllI {
+    size?: number;
+    bolder?: boolean;
+    lHeight?: number;
+}
+
 export const PrincipalDivLogin = styled.div`
     position: relative;
     width: 100vw;
@@ -25,8 +31,29 @@ export const PrincipalDivLogin = styled.div`
         background: #052554;
     } 
 `
+export const GridDivLogin = styled.div`
+    display: grid;
+    align-items: center;
+    justify-content: center;
+
+`
+
+
+export const FlexDivLogin = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 8vh;
+    margin-top: 6vh;
+
+    @media only screen and (max-width: 500px) {
+        margin-top: -15vh;
+    } 
+
+`
+
 export const ContainerLogin = styled.div`
-    position: relative;
+    position: relative;   
     width: 40vw;
     height: 60vh;
     border-radius: 15px;
@@ -44,7 +71,63 @@ export const HeaderLogin = styled.div`
     position: absolute;
     border: 1px solid lightblue;
     width: 100%;
-    height: 15%;
+    height: 30%;
+`
+export const HeaderLoginText = styled.p<TextControllI>`
+    font-size: ${(element: TextControllI) => element.size}vh;
+    color: #E66B25;
+    font-weight: ${(element: TextControllI) => element.bolder === true && 'bolder'};
+    height: 5vh;
+    line-height:  ${(element: TextControllI) => element.lHeight}%;
+
+
+/*     @media only screen and (max-width: 500px) {
+        font-size: 9px;
+    } 
+
+    @media only screen and (min-width: 1500px) {
+        font-size: 9px;
+    }
+
+    @media only screen and (min-width: 2000px) {
+        font-size: 12px;
+    } 
+
+    @media only screen and (min-width: 3000px) {
+        font-size: 20px;
+    } */
+
+`
+
+export const ContainerTop = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: flex-start; 
+    line-height: 100%;
+    font-size: 12px;
+`
+export const ContainerMed = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: flex-end;
+    font-size: 12px;
+    line-height: 100%;
+`
+
+export const ContainerBase = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: flex-end;
+    font-size: 12px;
+    line-height: 100%;
 `
 
 
+export const Container = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: baseline;
+    justify-content: space-between;
+    height: 7vh;
+`
