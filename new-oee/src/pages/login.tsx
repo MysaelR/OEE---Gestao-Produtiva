4ab1a3446eb694086acaccb91d37169acde4eb7b
MysaelR <90765797+MysaelR@users.noreply.github.com>
2022-06-08 16:57:11 -0300

    Criando tela de login responsiva

--- new-oee/src/pages/login.tsx
+++ new-oee/src/pages/login.tsx
@@ -1,45 +1,102 @@
-import { useState } from "react";
+import { FormHandles, SubmitHandler } from "@unform/core";
+import { useRef, useState } from "react";
+import * as Yup from 'yup';
+import User from '../assets/login/user.svg';
+import Password from '../assets/login/password.svg';
 
-import { Container, ContainerBase, ContainerLogin, ContainerMed, ContainerTop, FlexDivLogin, GridDivLogin, HeaderLogin, HeaderLoginText, PrincipalDivLogin } from "../styles/style-login";
+import { BodyLogin, ButtonLoginAndRegister, ContainerLogin, FlexDivLogin, FooterLogin, GridDivLogin, HeaderLogin, InputLogin, InputLoginAnchor, InputLoginContainerIcon, InputLoginIcon, MaxContainerLateralSpace, OptionLogin, OptionLoginSeparateText, OptionLoginText, PrincipalDivLogin, SystemLogo } from "../styles/style-login";
 
 export default function Login() {
+
+    const [selectedLogin, setSelectedLogin] = useState(true);
+
+    const formRef = useRef<FormHandles>(null);
+
+    const handleSubmit: SubmitHandler<FormData> = async data => {
+
+        try {
+            const schema = Yup.object().shape({
+                user: Yup.string().typeError("").required("Deve conter um usuário!"),
+                password: Yup.string().required("deve ter uma senha!"),
+            })
+            await schema.validate(data, {
+                abortEarly: false,
+            })
+
+            // Validation passed
+            console.log(data);
+        } catch (err) {
+            if (err instanceof Yup.ValidationError) {
+                // Validation failed
+                console.log(err.errors);
+            }
+        }
+
+
+    }
+
+
 
 
 
     return (
         <PrincipalDivLogin>
             <ContainerLogin>
+
+                {/*Cabeçalho da aplicação, contendo a logo do software, e a troca de login entre usuário e maquina */}
                 <HeaderLogin>
                     <GridDivLogin>
-                        <FlexDivLogin>
-                            <HeaderLoginText size={7} bolder lHeight={60}>
-                                OEE
-                            </HeaderLoginText>
-                            <HeaderLoginText size={1.72} lHeight={100}>
-                                SISTEMA DE<br />
-                                GESTÃO DA<br />
-                                PRODUÇÃO
-                            </HeaderLoginText>
+                        <FlexDivLogin top={6}>
+                            <SystemLogo />
                         </FlexDivLogin>
+                        <FlexDivLogin top={5} w={15}>
+                            <OptionLogin active={selectedLogin} bRight>
+                                <OptionLoginText onClick={() => { setSelectedLogin(true) }} active={selectedLogin}>
+                                    Usuário
+                                </OptionLoginText>
 
+                            </OptionLogin>
+                            <OptionLoginSeparateText active></OptionLoginSeparateText>
+                            <OptionLoginSeparateText></OptionLoginSeparateText>
+                            <OptionLogin active={!selectedLogin}>
+                                <OptionLoginText onClick={() => { setSelectedLogin(false) }} active={!selectedLogin}>
+                                    Máquina
+                                </OptionLoginText>
+                            </OptionLogin>
+                        </FlexDivLogin>
                     </GridDivLogin>
-
                 </HeaderLogin>
+
+
+                <BodyLogin>
+                    <MaxContainerLateralSpace>
+                        <InputLogin top={2}>
+                            <InputLoginContainerIcon>
+                                <InputLoginIcon src={User} />
+                            </InputLoginContainerIcon>
+                        </InputLogin>
+                        <InputLogin top={10}>
+                            <InputLoginContainerIcon>
+                                <InputLoginIcon src={Password} />
+                            </InputLoginContainerIcon>
+                        </InputLogin>
+                        <InputLoginAnchor>
+                            Esqueci minha senha
+                        </InputLoginAnchor>
+
+
+                    </MaxContainerLateralSpace>
+                </BodyLogin>
+                <FooterLogin>
+                    <MaxContainerLateralSpace>
+
+                        <ButtonLoginAndRegister>ENTRAR</ButtonLoginAndRegister>
+                    </MaxContainerLateralSpace>
+                </FooterLogin>
+
             </ContainerLogin>
         </PrincipalDivLogin>
     );
 }
 
 
-
-{/* <Container>
-<ContainerTop>
-    SISTEMA DE
-</ContainerTop>
-<ContainerMed>
-    GESTÃO DA
-</ContainerMed>
-<ContainerBase>
-    PRODUÇÃO
-</ContainerBase>
-</Container> */}
--- new-oee/src/styles/style-login.tsx
+++ new-oee/src/styles/style-login.tsx
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components';
+import { ReactComponent as Logo } from '../assets/login/logo.svg';
+import Background from '../assets/login/background.png';
 
-import Background from '../assets/login/background.png'
 
-interface InputControllI {
-    top?: number
+
+
+interface DivControllI {
+    top?: number;
+    w?: number;
 }
 
 interface TextControllI {
@@ -12,7 +16,30 @@ interface TextControllI {
     lHeight?: number;
 }
 
+interface OptionSelectedLoginI {
+    active?: boolean;
+    bRight?: boolean;
+}
+
+
+export const SystemLogo = styled(Logo)`
+    fill: #E66B25;
+    margin-top: 1vh;
+    width: 9vw;
+    @media only screen and (max-width: 600px) {
+        position: absolute;
+        margin-top: 110vh;
+        fill: #FFF;
+        width: 25vw;
+    }
+    @media only screen and (min-width: 2000px) {
+        height: 5vh;
+    }
+`
+
+
 export const PrincipalDivLogin = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
     position: relative;
     width: 100vw;
     height: 100vh;
@@ -23,6 +50,7 @@ export const PrincipalDivLogin = styled.div`
     background-image: url(${Background});
     background-repeat: no-repeat;
     background-size: cover;
+    font-family: 'Montserrat', sans-serif;
 
     @media only screen and (max-width: 450px) {
         /* background-repeat: no-repeat;
@@ -30,26 +58,6 @@ export const PrincipalDivLogin = styled.div`
         background-position-x: center; */
         background: #052554;
     } 
-`
-export const GridDivLogin = styled.div`
-    display: grid;
-    align-items: center;
-    justify-content: center;
-
-`
-
-
-export const FlexDivLogin = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 8vh;
-    margin-top: 6vh;
-
-    @media only screen and (max-width: 500px) {
-        margin-top: -15vh;
-    } 
-
 `
 
 export const ContainerLogin = styled.div`
@@ -67,67 +75,158 @@ export const ContainerLogin = styled.div`
     } 
 `
 
+export const MaxContainerLateralSpace = styled.div`
+    position: relative;
+    margin-left: 10%;
+    width: 80%;
+    height: 100%;
+    /* border: 1px solid black; */
+
+`
+
+//HEADER
 export const HeaderLogin = styled.div`
     position: absolute;
-    border: 1px solid lightblue;
+    /* border: 1px solid lightblue; */
     width: 100%;
-    height: 30%;
+    height: 20vh;
+    @media only screen and (max-width: 600px) {
+        height: 10vh;
+    } 
 `
-export const HeaderLoginText = styled.p<TextControllI>`
+
+export const GridDivLogin = styled.div`
+    display: grid;
+    align-items: center;
+    justify-content: center;
+
+`
+
+export const FlexDivLogin = styled.div<DivControllI>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 60%;
+    margin-top: ${(element: DivControllI) => element.top}vh;
+    width: ${(element: DivControllI) => element.w}vw;
+
+    @media only screen and (max-width: 600px) {
+
+    margin-top: ${(element: DivControllI) => (element.top && element.top * 0.6)}vh;
+    width: ${(element: DivControllI) => (element.w && element.w * 4)}vw;
+
+    } 
+`
+
+export const OptionLogin = styled.div<OptionSelectedLoginI>`
+    
+    height: 2vh;
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 3px;
+   
+
+
+`
+export const OptionLoginText = styled.p<OptionSelectedLoginI>`
+    color: ${(element: OptionSelectedLoginI) => element.active ? '#878A8C' : '#C7C7C7'};
+    font-weight: 600;
+    margin-top: -0.4vh;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+    border-bottom: ${(element: OptionSelectedLoginI) => element.active ? '0.3vh solid #E66B25' : 'none'};
+    border-collapse: separate;
+    border-spacing: 15px;
+    font-size: 2.2vh;
+
+`
+
+export const OptionLoginSeparateText = styled.div`
+    height: 85%;
+    border-right:${(element: OptionSelectedLoginI) => element.active && '0.3vh solid #C7C7C7'};
+    margin-top: -2vh;
+    width: 5%;
+  
+`
+
+//BODY
+export const BodyLogin = styled.div`
+    /* border: 1px solid red; */
+    margin-top: 20vh;
+    height: 20vh;
+    @media only screen and (max-width: 600px) {
+        margin-top: 10vh;
+        height: 20vh;
+    } 
+`
+
+export const InputLogin = styled.div<DivControllI>`
+    position: absolute;
+    display: flex;
+    align-items: center;
+
+    width: 99%;
+    border: 0.3vh solid #C7C7C7;
+    border-radius: 8px;
+    height: 4vh;
+    margin-top: ${(element: DivControllI) => element.top}vh;
+`
+export const InputLoginContainerIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 10%;
+    height: 70%;
+    border-right: 0.1vh solid #C7C7C7;
+    /* background-color: lightcyan; */
+`
+export const InputLoginIcon = styled.img`
+
+`
+export const InputLoginAnchor = styled.a`
+position: absolute;
+margin-top: 16vh;
+color: #878A8C;
+text-decoration-line: underline;
+`
+
+
+
+
+//FOOTER
+export const FooterLogin = styled.div`
+    /* border: 1px solid green; */
+    height: 20vh;
+    @media only screen and (max-width: 600px) {
+        height: 15vh;
+    } 
+`
+
+export const ButtonLoginAndRegister = styled.button`
+position: absolute;
+bottom: 3vh;
+background-color: #E66B25;
+width: 100%;
+padding: 1vh;
+border: none;
+color: white;
+border-radius: 8px;
+font-size: 2vh;
+
+`
+
+
+
+
+
+
+/* export const HeaderLoginText = styled.p<TextControllI>`
     font-size: ${(element: TextControllI) => element.size}vh;
     color: #E66B25;
     font-weight: ${(element: TextControllI) => element.bolder === true && 'bolder'};
     height: 5vh;
     line-height:  ${(element: TextControllI) => element.lHeight}%;
-
-
-/*     @media only screen and (max-width: 500px) {
-        font-size: 9px;
-    } 
-
-    @media only screen and (min-width: 1500px) {
-        font-size: 9px;
-    }
-
-    @media only screen and (min-width: 2000px) {
-        font-size: 12px;
-    } 
-
-    @media only screen and (min-width: 3000px) {
-        font-size: 20px;
-    } */
-
-`
-
-export const ContainerTop = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: flex-start; 
-    line-height: 100%;
-    font-size: 12px;
-`
-export const ContainerMed = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: flex-end;
-    font-size: 12px;
-    line-height: 100%;
-`
-
-export const ContainerBase = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: flex-end;
-    font-size: 12px;
-    line-height: 100%;
-`
-
-
-export const Container = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    align-items: baseline;
-    justify-content: space-between;
-    height: 7vh;
-`
+` */
